ad8d70a76a352a9b4e81c1d534ee77db9bc72699
LorenFiorini <lorenfiorini@gmail.com>
2022-06-03 15:44:52 +0200

    Friday

--- C00/day1.c
+++ C00/day1.c
@@ -6,11 +6,21 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 12:34:38 by lfiorini          #+#    #+#             */
-/*   Updated: 2022/06/02 16:40:26 by lfiorini         ###   ########.fr       */
+/*   Updated: 2022/06/03 15:44:06 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main() {
+#include<unistd.h>
+
+void ft_putchar(char c) { write(1, &c, 1); }
+
+void ft_nputchar(char c, int n) {while (n--) ft_putchar(c);}
+
+
+int main() 
+{
+	ft_nputchar('*', 7);
+	ft_putchar('\n');
 	
 	return 0;
 }
